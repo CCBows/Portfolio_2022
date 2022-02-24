fdc9976159082dae5bac618dcdc198bd257449b1
Carlos <arcos.moreno94@gmail.com>
2022-02-24 14:55:15 +0100

    JS main content hecho
    JS main content hecho, falta revisar el menu de navegacion de contenidos y bajar de peso las imágenes.

--- js/funciones.js
+++ js/funciones.js
@@ -6,13 +6,51 @@ $(document).ready(function(){
         $("#desplegable").addClass("show");
         $("#close").addClass("show-close");
         $("nav#menu #burger-menu").css("display","none");
-
     });
     $("#close").click (function(){
         $("#desplegable").removeClass("show");
         $("#close").removeClass("show-close");
         $("nav#menu #burger-menu").css("display","flex");
-
+    });
+    $(".item-1").click(function(){
+        $(".super-container").addClass("go-item-1").removeClass("go-item-2").removeClass("go-item-3").removeClass("go-item-4").removeClass("go-item-5");
+        $(".item-1").addClass("selected-item");
+        $(".item-2").removeClass("selected-item");
+        $(".item-3").removeClass("selected-item");
+        $(".item-4").removeClass("selected-item");
+        $(".item-5").removeClass("selected-item");
+    });
+    $(".item-2").click(function(){
+        $(".super-container").addClass("go-item-2").removeClass("go-item-1").removeClass("go-item-3").removeClass("go-item-4").removeClass("go-item-5");
+        $(".item-2").addClass("selected-item");
+        $(".item-1").removeClass("selected-item");
+        $(".item-3").removeClass("selected-item");
+        $(".item-4").removeClass("selected-item");
+        $(".item-5").removeClass("selected-item");
+    });
+    $(".item-3").click(function(){
+        $(".super-container").addClass("go-item-3").removeClass("go-item-2").removeClass("go-item-1").removeClass("go-item-4").removeClass("go-item-5");
+        $(".item-3").addClass("selected-item");
+        $(".item-1").removeClass("selected-item");
+        $(".item-2").removeClass("selected-item");
+        $(".item-4").removeClass("selected-item");
+        $(".item-5").removeClass("selected-item");
+    });
+    $(".item-4").click(function(){
+        $(".super-container").addClass("go-item-4").removeClass("go-item-2").removeClass("go-item-3").removeClass("go-item-1").removeClass("go-item-5");
+        $(".item-4").addClass("selected-item");
+        $(".item-1").removeClass("selected-item");
+        $(".item-2").removeClass("selected-item");
+        $(".item-3").removeClass("selected-item");
+        $(".item-5").removeClass("selected-item");
+    });
+    $(".item-5").click(function(){
+        $(".super-container").addClass("go-item-5").removeClass("go-item-2").removeClass("go-item-3").removeClass("go-item-4").removeClass("go-item-1");
+        $(".item-5").addClass("selected-item");
+        $(".item-1").removeClass("selected-item");
+        $(".item-2").removeClass("selected-item");
+        $(".item-3").removeClass("selected-item");
+        $(".item-4").removeClass("selected-item");
     });
 
 });
